20af78bc8ed23d510da3fee565256bf463c66373
Useit14 <useit14@inbox.ru>
2022-06-13 18:09:35 +0300

    Level 2 and 3 are complete

--- js.js
+++ js.js
@@ -1,15 +1,77 @@
-function dropNavigationClick(event){
-    let element=event.getElementsByTagName('img')[0];
-    let array=element.src.split("/")
-    let name=array[array.length-1]
-    if(name=="arrowDown.png"){
-        element.src="/images/arrowUp.png"
-    }
-    else{
-        element.src="/images/arrowDown.png"
-    }
-   
-
-    
-    
+function dropNavigationClick(event) {
+  let element = event.getElementsByTagName("img")[0];
+  let array = element.src.split("/");
+  let name = array[array.length - 1];
+  if (name == "arrowDown.png") {
+    element.src = "/images/arrowUp.png";
+  } else {
+    element.src = "/images/arrowDown.png";
+  }
 }
+function mapLoad() {
+  let preloader = document.getElementsByClassName("map-preloader")[0];
+  // let map=document.getElementsByClassName("map")[0]
+  preloader.classList.add("map-preloader-hidden");
+}
+
+function setTimer() {
+    sessionStorage.setItem("hour", "0");
+    sessionStorage.setItem("minutes", "0");
+    sessionStorage.setItem("seconds", "0");
+  sessionStorage.setItem("IsSetTimer", "true");
+  upTimer();
+}
+
+function upTimer() {
+  let title = document.title;
+  let hour = document.getElementById("hour");
+  let seconds = document.getElementById("seconds");
+  let minutes = document.getElementById("minutes");
+  setInterval(function () {
+    let countHour = parseInt(sessionStorage.getItem("hour"));
+    let countMinutes = parseInt(sessionStorage.getItem("minutes"));
+    let countSeconds = parseInt(sessionStorage.getItem("seconds"));
+    if (title == "Timer") {
+      hour.textContent = countHour;
+      minutes.textContent = countMinutes;
+      seconds.textContent = countSeconds;  
+    }
+    if (countSeconds < 59) {
+      countSeconds++;
+      if (title == "Timer") {
+        seconds.textContent = countSeconds;
+      }
+    } else if (countMinutes < 59) {
+      
+      countMinutes++;
+      if (title == "Timer") {
+        seconds.textContent=0
+        minutes.textContent = countMinutes;
+        
+      }
+      countSeconds = 0;
+      
+    } else{ 
+      countHour++;
+        if (title == "Timer") {
+            seconds.textContent=0
+            minutes.textContent = 0;  
+            hour.textContent = countHour;
+        }
+       
+      countSeconds=0
+      countMinutes=0
+      
+    }
+    
+    sessionStorage.setItem("hour", countHour);
+    sessionStorage.setItem("minutes", countMinutes);
+    sessionStorage.setItem("seconds", countSeconds);
+  }, 1000);
+}
+
+window.addEventListener("load", function () {
+  if (sessionStorage.getItem("IsSetTimer")) {
+    upTimer();
+  }
+});
