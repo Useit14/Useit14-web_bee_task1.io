7ef9ea22bb7aa3ee08f718a709bbef14d75d351d
Useit14 <useit14@inbox.ru>
2022-07-16 22:52:52 +0300

    All fix

--- js/js.js
+++ js/js.js
@@ -1,8 +1,16 @@
+/* eslint-disable require-jsdoc */
+
+// eslint-disable-next-line require-jsdoc
+
+let countSeconds = 0;
+let timerId = 0;
+
+window.addEventListener("DOMContentLoaded", instalEventColors);
 
 function dropNavigationClick(event) {
-  let element = event.getElementsByTagName("img")[0];
-  let array = element.src.split("/");
-  let name = array[array.length - 1];
+  const element = event.getElementsByTagName("img")[0];
+  const array = element.src.split("/");
+  const name = array[array.length - 1];
   if (name == "arrowDown.png") {
     element.src = "../images/arrowUp.png";
   } else {
@@ -10,9 +18,9 @@ function dropNavigationClick(event) {
   }
 }
 function dropIndexNavigationClick(event) {
-  let element = event.getElementsByTagName("img")[0];
-  let array = element.src.split("/");
-  let name = array[array.length - 1];
+  const element = event.getElementsByTagName("img")[0];
+  const array = element.src.split("/");
+  const name = array[array.length - 1];
   if (name == "arrowDown.png") {
     element.src = "images/arrowUp.png";
   } else {
@@ -20,33 +28,63 @@ function dropIndexNavigationClick(event) {
   }
 }
 function mapLoad() {
-
-  let preloader = document.getElementsByClassName("map-preloader")[0];
-  // let map=document.getElementsByClassName("map")[0]
+  const preloader = document.getElementsByClassName("map-preloader")[0];
   preloader.classList.add("map-preloader-hidden");
 }
 
-let countSeconds=0
 function startTimer() {
-  setInterval(function(){
-    printTimer()
-    countSeconds++
-  },1000)
+  timerId = setInterval(function () {
+    countSeconds++;
+    printTimer();
+  }, 1000);
 }
 
-function printTimer(){
-  let seconds=document.getElementById('seconds') 
-  let minutes=document.getElementById('minutes')
-  let hour=document.getElementById('hour')
-  hour.textContent=Math.floor(countSeconds/3600) //0 
-  minutes.textContent= Math.floor((countSeconds-hour.textContent*3600)/60) //0
-  seconds.textContent=countSeconds-(hour.textContent*3600+minutes.textContent*60)
-
+function printTimer() {
+  const seconds = document.getElementById("seconds");
+  const minutes = document.getElementById("minutes");
+  const hour = document.getElementById("hour");
+  if (pageDate.filename == "timer.html") {
+    hour.textContent = Math.floor(countSeconds / 3600); // 0
+    minutes.textContent = Math.floor(
+      (countSeconds - hour.textContent * 3600) / 60
+    ); 
+    seconds.textContent =
+      countSeconds - (hour.textContent * 3600 + minutes.textContent * 60);
+  }
 }
 
-window.addEventListener('hashchange',function(){
- if(this.document.title=='Timer'){
-  startTimer()
- }
-})
+function refreshTimer() {
+  clearInterval(timerId);
+  countSeconds = 0;
+  startTimer();
+}
 
+function instalEventColors() {
+  const submenu = document.getElementsByClassName("submenu");
+  for (let index = 0; index < submenu.length; index++) {
+    const menu = submenu[index];
+    const navLinks = menu.getElementsByClassName("nav-item");
+    for (let index = 0; index < navLinks.length; index++) {
+      const element = navLinks[index];
+      element.addEventListener("click", changeBgColor);
+    }
+  }
+}
+
+function changeBgColor(event) {
+  clearBgColor();
+  const element = event.path[2];
+  element.classList.add("bg-secondary");
+}
+
+function clearBgColor() {
+  const submenu = document.getElementsByClassName("submenu");
+  for (let index = 0; index < submenu.length; index++) {
+    const menu = submenu[index];
+    const navLinks = menu.getElementsByClassName("nav-item");
+    for (let index = 0; index < navLinks.length; index++) {
+      const element = navLinks[index];
+      element.classList.remove("bg-secondary");
+    }
+  }
+}
--- js/router.js
+++ js/router.js
@@ -1,38 +1,54 @@
+/* eslint-disable require-jsdoc */
+// eslint-disable-next-line require-jsdoc
+const pageDate = {
+  path: 'pages/index.html',
+  title: 'Index',
+};
 
-function locationChangeByLoad(event){
-    event.preventDefault()
-    let location=event.target['baseURI']
-    let htmlName=location.split('/')
-    htmlName=htmlName[htmlName['length']-1] 
-    let name=sessionStorage.getItem('name')
-    name==null?htmlName=htmlName:htmlName=name
-    history.pushState({},'','index.html')
-    history.pushState({},'','pages/'+htmlName)
-    let title
-    htmlName=='index.html'?title='Главная':htmlName=='map.html'?title='Map':title='Timer'
-    document.title=title
-    sendRequet(htmlName).then(data=>loadCode(data))
-}
-function locationChangeByLink(event,location){
-    event.preventDefault();
-    let htmlName
-    htmlName=location.split('/')
-    htmlName=htmlName[htmlName['length']-1] 
-    history.pushState({},'',htmlName)
-    let title
-    htmlName=='index.html'?title='Главная':htmlName=='map.html'?title='Map':title='Timer'
-    document.title=title
-    sendRequet(htmlName).then(data=>loadCode(data))
-}
-function sendRequet(htmlName){
-    let app=document.getElementById("app")
-    return fetch(htmlName).then(respone=>{
-      return respone.text()
-    })
+function locationChangeByLoad(event) {
+  event.preventDefault();
+  getDate();
+  setTitle();
+  history.pushState({ name: pageDate.filename }, '', pageDate.path);
+  sendRequet(pageDate.filename).then((data) => loadCode(data));
 }
 
-function loadCode(response){
-  let app=document.getElementById("app")
-  app.innerHTML=response
+function locationChangeByLink(_event, filename) {
+  _event.preventDefault();
+  history.pushState({ name: filename }, '', filename);
+  getDate();
+  setTitle();
+  sendRequet(filename).then((data) => loadCode(data));
 }
 
+function getDate() {
+  if (history.state) {
+    pageDate.path = history.state['name'];
+  }
+  getFileName();
+  title = pageDate.filename;
+  title = title.split('.');
+  title = title[0].toUpperCase();
+  pageDate.title = title;
+}
+
+function getFileName() {
+  let filename = pageDate.path;
+  filename = filename.toString().split('/');
+  pageDate.filename = filename[filename.length - 1];
+}
+
+function setTitle() {
+  document.title = pageDate.title;
+}
+
+function sendRequet(htmlName) {
+  return fetch(htmlName).then((respone) => {
+    return respone.text();
+  });
+}
+
+function loadCode(response) {
+  const app = document.getElementById('app');
+  app.innerHTML = response;
+}
